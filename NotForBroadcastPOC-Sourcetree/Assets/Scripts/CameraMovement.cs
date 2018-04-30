640276de4f99dacc487f1e133a657543fef8a9f5
jasonorbaum <jay@notgames.co.uk>
2018-04-30 16:57:42 +0100

    Keyboard controlled camera implemented Blinds re-limited for new views and left desk positions adjusted

--- NotForBroadcastPOC-Sourcetree/Assets/Scripts/CameraMovement.cs
+++ NotForBroadcastPOC-Sourcetree/Assets/Scripts/CameraMovement.cs
@@ -4,20 +4,93 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour {
 
+    public bool freeLook = false;
     public float speedH = 2.0f;
     public float speedV = 2.0f;
 
     private float yaw = 0.0f;
     private float pitch = 0.0f;
+    private Animator myAnimator;
+    private string myPosition="Centre";
+
+    private void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+        if (freeLook)
+        {
+            myAnimator.enabled = false;
+            // Move Camera and set Depth of Field
+        }
+    }
 
     void Update()
     {
-        if (Input.GetMouseButton(1))
+        if (freeLook)
         {
-            yaw += speedH * Input.GetAxis("Mouse X");
-            pitch -= speedV * Input.GetAxis("Mouse Y");
+            // Right Click Mouse Control
 
-            transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+            if (Input.GetMouseButton(1))
+            {
+                yaw += speedH * Input.GetAxis("Mouse X");
+                pitch -= speedV * Input.GetAxis("Mouse Y");
+
+                transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+            }
+        } else
+        {
+            // Keyboard Controls
+            if (Input.GetButtonDown("Down"))
+            {
+                if (myPosition!="Down")
+                {
+                    myAnimator.SetTrigger("downPressed");
+                    myPosition = "Down";
+                }
+            }
+
+            if (Input.GetButtonDown("Up"))
+            {
+                if (myPosition == "Down")
+                {
+                    myAnimator.SetTrigger("upPressed");
+                    myPosition = "Centre";
+                }
+            }
+
+            if (Input.GetButtonDown("Left"))
+            {
+                if (myPosition != "Left")
+                {
+                    myAnimator.SetTrigger("leftPressed");
+                    if (myPosition == "Right")
+                    {
+                        myPosition = "Centre";
+                    }
+                    else
+                    {
+                        myPosition = "Left";
+                    }
+                }
+            }
+
+            if (Input.GetButtonDown("Right"))
+            {
+                if (myPosition != "Right")
+                {
+                    myAnimator.SetTrigger("rightPressed");
+                    if (myPosition == "Left")
+                    {
+                        myPosition = "Centre";
+                    }
+                    else
+                    {
+                        myPosition = "Right";
+                    }
+                }
+            }
+
         }
+
     }
+
 }
