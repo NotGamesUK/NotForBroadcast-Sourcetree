33d7a9de0d35b80142edeb215337544a837e2aa0
jasonorbaum <jay@notgames.co.uk>
2018-05-05 00:28:48 +0100

    Mixing Desk Works!!!

---
+++ NotForBroadcastPOC-Sourcetree/Assets/Scripts/MuteControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuteControl : MonoBehaviour {
+
+    public int myID;
+    private ButtonAnimating myButton;
+    private bool lastCheck;
+    private SoundDesk myDesk;
+
+	// Use this for initialization
+	void Start () {
+        myButton = GetComponent<ButtonAnimating>();
+        myDesk = FindObjectOfType<SoundDesk>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (lastCheck != myButton.isDepressed)
+        {
+            Debug.Log("State of button changed.");
+            lastCheck = myButton.isDepressed;
+            if (myButton.hasPower)
+            {
+                if (lastCheck == true)
+                {
+                    myDesk.MuteChannel(myID);
+                }
+                else
+                {
+                    myDesk.UnmuteChannel(myID);
+                }
+            }
+        }
+
+    }
+}
---
+++ NotForBroadcastPOC-Sourcetree/Assets/Scripts/SoundDesk.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+public class SoundDesk : MonoBehaviour {
+
+    public GroovedSlider[] mySliders;
+    public ButtonAnimating[] myMutes;
+    public AudioMixer myDesk;
+    public ButtonAnimating myBleepButton;
+    public bool hasPower=false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public void MuteChannel (int thisChannel)
+    {
+        Debug.Log("Call to mute Channel: " + thisChannel);
+        string channelRequired = "Screen0" + thisChannel + "Vol";
+        Debug.Log(channelRequired);
+        myDesk.SetFloat(channelRequired, -80f);
+    }
+
+    public void UnmuteChannel (int thisChannel)
+    {
+        Debug.Log("Call to Unmute Channel: " + thisChannel);
+        string channelRequired = "Screen0" + thisChannel + "Vol";
+        Debug.Log(channelRequired);
+        myDesk.SetFloat(channelRequired, 0f);
+
+    }
+
+    public void SetMasterVolume(float thisVolume)
+    {
+        thisVolume = 1 - thisVolume;
+        myDesk.SetFloat("MasterVol", thisVolume*-80f);
+    }
+
+    void PowerOn()
+    {
+        hasPower = true;
+        foreach (GroovedSlider thisSlider in mySliders)
+        {
+            thisSlider.hasPower = true;
+        }
+        foreach (ButtonAnimating thisMute in myMutes)
+        {
+            thisMute.hasPower = true;
+        }
+        myBleepButton.hasPower = true;
+    }
+
+    void PowerOff()
+    {
+        hasPower = false;
+        foreach (GroovedSlider thisSlider in mySliders)
+        {
+            thisSlider.hasPower = false;
+        }
+        foreach (ButtonAnimating thisMute in myMutes)
+        {
+            thisMute.hasPower = false;
+        }
+        myBleepButton.hasPower = false;
+    }
+}
+
